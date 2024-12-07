3ce0be5a3f3bc3df7213579b6cd2de4eb3930077
Emirhan Topal <67663069+EmirhanTopal@users.noreply.github.com>
2024-12-07 15:20:17 +0300

    Add files via upload

---
+++ get_next_line_bonus.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emtopal <emtopal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 18:16:39 by emtopal           #+#    #+#             */
+/*   Updated: 2024/12/07 15:05:28 by emtopal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static	void	*only_free(char **buffer, char **adrs)
+{
+	free(*buffer);
+	free(*adrs);
+	*adrs = NULL;
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*p2;
+
+	i = 0;
+	p2 = (char *) malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (p2 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		p2[i] = s1[i];
+		i++;
+	}
+	p2[i] = '\0';
+	return (p2);
+}
+
+static	char	*reminder_value(char **str)
+{
+	char	*line;
+	char	*temp;
+	int		len;
+
+	len = 0;
+	if (!*str)
+	{
+		free(*str);
+		*str = NULL;
+		return (NULL);
+	}
+	while ((*str)[len] != '\0')
+	{
+		if ((*str)[len] == '\n')
+		{
+			len++;
+			break ;
+		}
+		len++;
+	}
+	line = ft_strndup(*str, len);
+	temp = ft_strdup(*str + len);
+	free(*str);
+	*str = temp;
+	return (line);
+}
+
+static	char	*read_file(int *fd, char *adrs)
+{
+	char	*buffer;
+	char	*temp;
+	int		bytes_count;
+
+	buffer = (char *) malloc((BUFFER_SIZE + 1));
+	bytes_count = 0;
+	if (buffer == NULL)
+		return (NULL);
+	if (adrs == NULL)
+		adrs = ft_strdup("");
+	while (ft_strchr(adrs, '\n') == NULL && bytes_count >= 0)
+	{
+		bytes_count = read(*fd, buffer, BUFFER_SIZE);
+		if (bytes_count < 0)
+			return (only_free (&buffer, &adrs));
+		buffer[bytes_count] = '\0';
+		if (bytes_count == 0)
+			break ;
+		temp = ft_strjoin(adrs, buffer);
+		free(adrs);
+		adrs = temp;
+	}
+	free(buffer);
+	return (adrs);
+}
+
+char	*get_next_line_bonus(int *fd)
+{
+	static char	*last_pointer = NULL;
+	char		*new_line;
+
+	if (*fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	last_pointer = read_file(fd, last_pointer);
+	if (!last_pointer || last_pointer[0] == '\0')
+	{
+		free(last_pointer);
+		last_pointer = NULL;
+		return (NULL);
+	}
+	new_line = reminder_value(&last_pointer);
+	return (new_line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emtopal <emtopal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 14:32:24 by emtopal           #+#    #+#             */
+/*   Updated: 2024/12/07 14:38:06 by emtopal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line_bonus(int *fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strndup(const char *s, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
